4ef26ca92b77352e3a4b1653561087444de7fd39
KhanKieu <kgowl.0260@KhanKieus-MacBook-Pro.local>
2025-03-13 19:39:33 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/* document.addEventListener("DOMContentLoaded", function() {
+    alert("Welcome to my portfolio!");
+}); 
+function changeBackground() {
+    document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+} */
