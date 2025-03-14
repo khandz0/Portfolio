3e99e7b57a6da562d491f4ffba9562fc03489284
KhanKieu <kgowl.0260@KhanKieus-MacBook-Pro.local>
2025-03-13 21:37:41 -0700

    Forcing GitHub Pages to update index.html

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 /* document.addEventListener("DOMContentLoaded", function() {
     alert("Welcome to my portfolio!");
-}); 
+}); */
 function changeBackground() {
     document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-} */
+} 
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("dark-mode-toggle");
+    toggleButton.addEventListener("click", function() {
+        document.body.classList.toggle("dark-mode");
+    });
+});
